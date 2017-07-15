bd684e762e8da925e4637481f4f181f25da644e3
Ove Sundal <osu002@post.uit.no>
2017-07-15 17:34:46 +0200

    feat: Add frogger project. Add shadowing effect for clickable pictures/icons

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,20 @@
-$("#resume-image").click(function() {
-  window.location.href = "https://ovsundal.github.io/frontend-nanodegree-resume/";
-});
+$(document).ready(function() {
 
-$("#github-icon").click(function() {
-  window.location.href = "https://github.com/ovsundal";
+    $(".cursor-link").hover(function(){ $(this).toggleClass('shadow-effect'); });
+
+
+    $("#resume-image").click(function () {
+        window.open("https://ovsundal.github.io/Resume/");
+    });
+    $("#frogger-image").click(function () {
+        window.open("https://ovsundal.github.io/Frogger-Arcade-Game/");
+    });
+
+    $("#github-icon").click(function () {
+        window.open("https://github.com/ovsundal");
+    });
+
+    $("#linkedIn-icon").click(function () {
+        window.open("https://www.linkedin.com/in/ove-sundal-08172128/");
+    });
 });
