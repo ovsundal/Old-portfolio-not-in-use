f0031207471c6ac69882b707f13369c9841967f1
Ove Sundal <osundal83@gmail.com>
2017-08-12 16:02:40 +0200

    feat: add project info and screenshots

--- src/js/script.js
+++ src/js/script.js
@@ -1,50 +1,64 @@
-let ViewModel = function () {
+let projects = [
+    {
+        pictureUrl: "src/images/feed-reader-testing-450w.jpg",
+        title: "Feed Reader Testing",
+        keywords: "jasmine framework, unit testing",
+        projectUrl: "https://ovsundal.github.io/Feed-Reader-Testing/",
+        introduction: "Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsum",
+        learningOutcomes: "Jasmine testing framework, async unit testing"
+    },
+    {
+        pictureUrl: "src/images/project2.jpeg",
+        title: "Neigborhood Map of Stavanger",
+        keywords: "knockoutJs (MVVM), async API requests, google maps",
+        projectUrl: "https://github.com/ovsundal/Neighborhood-Map-of-Stavanger",
+        introduction: "Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsum",
+        learningOutcomes: "learningOutcomes",
+    },
+    {
+        pictureUrl: "src/images/website-optimization-450w.jpg",
+        title: "Website Optimisation",
+        keywords: "gulp task runner, file minification, google pagespeed",
+        projectUrl: "https://ovsundal.github.io/Frogger-Arcade-Game/",
+        introduction: "Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsum",
+        learningOutcomes: "learningOutcomes",
+    },
+    {
+        pictureUrl: "src/images/Frogger-image.jpg",
+        title: "Frogger Arcade Game",
+        keywords: "OOP, HTML5 Canvas",
+        projectUrl: "https://ovsundal.github.io/Frogger-Arcade-Game/",
+        introduction: "Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsum",
+        learningOutcomes: "learningOutcomes",
+    }
+];
 
-    let self = this;
+//create project
+projects.forEach(function (project) {
 
-    self.projectList = ko.observableArray([
-        {
-            title: "Neighborhood Map",
-            introduction: "Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsum",
-            learningOutcomes: "learningOutcomes",
-            keywords: "keyword1, keyword2",
-            pictureUrl: "PICTURE"
-        },
-        {
-            title: "Feed Reader Testing",
-            introduction: "Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsum",
-            learningOutcomes: "learningOutcomes",
-            keywords: "keyword1, keyword2",
-            pictureUrl: "PICTURE"
-        },
-        {
-            title: "Frogger Arcade Game",
-            introduction: "Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsum",
-            learningOutcomes: "learningOutcomes",
-            keywords: "keyword1, keyword2",
-            pictureUrl: "PICTURE"
-        },
-        {
-            title: "Website Performance Optimisation",
-            introduction: "Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsum",
-            learningOutcomes: "learningOutcomes",
-            keywords: "keyword1, keyword2",
-            pictureUrl: "PICTURE"
-        }
-    ]);
-};
+    $('#projectHolder')
+        .append(
+            '<article class="col-sm-3">' +
+            '<img id="project.title" src="' + project.pictureUrl + '" class="img-responsive cursor-link" alt="Image" />' +
+            '<h3 class="text-center project-title">' + project.title + "</h3>" +
+            '<div class="text-center"><i>' + project.keywords + '</i></div>' +
+            '</article>'
+        );
 
-ko.applyBindings(new ViewModel());
+    $("#" + project.title).click(function () {
+        window.open(project.name);
+    });
 
-$(document).ready(function() {
+});
 
-    //shadow effect when hovering
-    $(".cursor-link").hover(
-        function(){ $(this).removeClass('shadow-effect') },
 
-        function() { $(this).toggleClass('shadow-effect')});
+$(document).ready(function () {
 
-    //link references
+    $(".cursor-link").hover(function () {
+        $(this).toggleClass('shadow-effect');
+    });
+
+
     $("#resume-image").click(function () {
         window.open("https://ovsundal.github.io/Resume/");
     });
